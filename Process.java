f990a28d539274e7f3e08ae295106531faa5ead4
Taina Conde <tainabatistaconde@gmail.com>
2021-12-09 10:45:33 -0500

    feat: Create Process class

---
+++ Process.java
@@ -0,0 +1,44 @@
+public class Process {
+
+    private int id;
+    private int duration;
+    private int arrivalTime;
+    private int priority;
+
+    public Process() {
+    }
+
+    public Process(int id, int priority, int duration, int arrivalTime) {
+        this.id = id;
+        this.priority = priority;
+        this.duration = duration;
+        this.arrivalTime = arrivalTime;
+    }
+    //getters
+    public int getProcessId() { return id; }
+    public int getPriority() { return priority; }
+    public int getDuration() { return duration; }
+    public int getArrivalTime() { return arrivalTime; }
+    //setters
+    public void setProcessId(int id) {
+        this.id = id ;
+    }
+    public void setPriority(int priority){
+        this.priority = priority;
+    }
+    public void setDuration(int duration){
+        this.duration = duration;
+    }
+    public void setArrivalTime(int arrivalTime){
+        this.arrivalTime = arrivalTime;
+    }
+
+    public String toString() {
+        String c =
+                "Id = " + id + ", priority = " + priority + ", duration = " + duration
+                        + ", arrival time = " + arrivalTime;
+
+        return c;
+    }
+
+}
--- ProcessScheduling.java
+++ ProcessScheduling.java
@@ -1,5 +1,42 @@
+import java.util.ArrayList;
 class ProcessScheduling {
+    /**
+     *
+     * */
+    private static ArrayList readProcesses() {
+        ArrayList D = new ArrayList();
+        // read input file and store all process Objects in the D
+        File myFile = new File("process_scheduling_input.txt");
+        Scanner myReader = new Scanner(myFile);
+        // initialize an index that will be used to store process instances in D
+        int index = 0;
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            // split the data string on the whitespace delimiter
+            String[] tokens = data.split(" ");
+            // eliminate whitespaces in each token. Convert them to integers
+            int processId = Integer.parseInt(tokens[0].trim());
+            int priority = Integer.parseInt(tokens[1].trim());
+            int duration = Integer.parseInt(tokens[2].trim());
+            int arrivalTime = Integer.parseInt(tokens[3].trim());
+
+            Car newCar = new Car(make, year, price);
+            cars[index] = newCar;
+            index++;
+
+        }
+        myReader.close();
+
+    }
     public static void main(String[] args) {
-        
+        //Read all processes from an input file and store them in an appropriate data structure, D
+        // initialize currentTime
+        //set running initially to false
+        // create an empty priority queue Q
+
+        // Each iteration of the while loop represents what occurs during one time unit
+        // Must increment currentTime in each iteration
+
+
     }
 }
