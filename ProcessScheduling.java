8158c33f388ba5720d8e5fbe207e07f062ada882
Taina Conde <tainabatistaconde@gmail.com>
2021-12-10 11:04:55 -0500

    feat: write to output file

--- ProcessScheduling.java
+++ ProcessScheduling.java
@@ -3,6 +3,7 @@ import java.util.PriorityQueue;
 import java.io.File;
 import java.util.Scanner;
 import java.io.IOException;
+import java.io.FileWriter;
 
 class ProcessScheduling {
     /** readProcess reads the input file that stores information about all processes. It creates Process instances
@@ -40,20 +41,26 @@ class ProcessScheduling {
      * @param executingProcess the Process that is currently running, or just finished running.
      * @param Q the PriorityQueue that stores the processes that arrived at the system and are waiting to run according
      *          to their priority.
+     * @throws IOException when an error occurs when writing to an output file.
      * */
-    private static void completeExecution(int currTime, int maxWait, Process executingProcess, PriorityQueue<Process> Q) {
-        System.out.println("Process "
+    private static void completeExecution(
+            int currTime,
+            int maxWait,
+            Process executingProcess,
+            PriorityQueue<Process> Q,
+            FileWriter myWriter) throws IOException{
+        myWriter.write("Process "
                 + executingProcess.getProcessId() +
                 " finished at time "
                 + currTime +
                 "\n"
         );
         // Update priorities of processes that have been waiting longer than max. wait time
-        System.out.println("Update priority: ");
+        myWriter.write("Update priority: ");
         for (Process process: Q) {
             int waitTime = currTime - process.getArrivalTime();
             if (waitTime > maxWait) {
-                System.out.println("PID = "
+                myWriter.write("PID = "
                         + process.getProcessId() +
                         " , wait time = "
                         + waitTime +
@@ -62,26 +69,38 @@ class ProcessScheduling {
 
                 );
                 process.setPriority(process.getPriority() - 1);
-                System.out.println("PID = "
+                myWriter.write("PID = "
                         + process.getProcessId() +
                         " , new priority = "
-                        + process.getPriority() 
+                        + process.getPriority()
                 );
             }
         }
 
     }
-    private static void printExecutingMessages(int currTime, int waitTime, double totalWaitTime, Process executingProcess) {
-        System.out.println("\nProcess removed from queue is: id = "
+    /** this method prints the messages about the process that started to run according to the expected output format.
+     * @param currTime the integer that represents the current time.
+     * @param waitTime the time the process waits from arrival until it is executed.
+     * @param totalWaitTime the sum of wait times of all the processes that were executed.
+     * @param executingProcess the process that is currently running.
+     * @param myWriter  write to an output file
+     * @throws IOException if an error occurs when writing to an output file.
+     * */
+    private static void printExecutingMessages(
+            int currTime, int waitTime,
+            double totalWaitTime,
+            Process executingProcess,
+            FileWriter myWriter) throws IOException {
+
+        myWriter.write("\nProcess removed from queue is: id = "
                 + executingProcess.getProcessId() +
                 " , at time "
                 + currTime +
                 " , wait time = "
                 + waitTime +
                 " Total wait time = "
-                + totalWaitTime
-        );
-        System.out.println("Process id = "
+                + totalWaitTime);
+        myWriter.write("Process id = "
                 + executingProcess.getProcessId() +
                 "\n\t\tPriority = "
                 + executingProcess.getPriority() +
@@ -114,12 +133,14 @@ class ProcessScheduling {
         // PriorityQueue Q that orders its elements according to the comparator (lowest priority).
         PriorityQueue<Process> Q = new PriorityQueue<>( new ProcessComparator());
 
-        //First, print all processes
+        //First, print all processes in an output file
+        FileWriter myWriter = new FileWriter("process_scheduling_output.txt");
         for (Process p: D) {
-            System.out.println(p);
+            myWriter.write(p.toString());
         }
+
         //print maximum wait time
-        System.out.println("\nMaximum wait time = " + maxWait);
+        myWriter.write("\nMaximum wait time = " + maxWait);
         // Each iteration of the while loop represents what occurs during one time unit
         while(!D.isEmpty()) {
             // look in all processes stored in D
@@ -138,7 +159,7 @@ class ProcessScheduling {
                 // set running back to false, the process that was running just reached the duration of its execution
                 //and finished
                 running = false;
-                completeExecution(currTime, maxWait, executingProcess, Q);
+                completeExecution(currTime, maxWait, executingProcess, Q, myWriter);
 
             }
             // if no process is running and there still processes waiting in the queue,
@@ -147,7 +168,7 @@ class ProcessScheduling {
                 executingProcess = Q.poll();
                 int waitTime = currTime - executingProcess.getArrivalTime();
                 totalWaitTime += waitTime;
-                printExecutingMessages(currTime, waitTime, totalWaitTime, executingProcess);
+                printExecutingMessages(currTime, waitTime, totalWaitTime, executingProcess, myWriter);
 
                 // set the executingDuration variable to mark the duration of the process that is now running
                 executingDuration = executingProcess.getDuration();
@@ -174,21 +195,20 @@ class ProcessScheduling {
                 // set running back to false, the process that was running just reached the duration of its execution
                 //and finished
                 running = false;
-                completeExecution(currTime,maxWait, executingProcess, Q );
+                completeExecution(currTime,maxWait, executingProcess, Q, myWriter);
             }
+            // execute the next process from the priority queue if there is no process running
             if (!running) {
                 executingProcess = Q.poll();
                 int waitTime = currTime - executingProcess.getArrivalTime();
                 totalWaitTime += waitTime;
-                printExecutingMessages(currTime, waitTime, totalWaitTime, executingProcess);
+                printExecutingMessages(currTime, waitTime, totalWaitTime, executingProcess, myWriter);
                 // set the executingDuration variable to mark the duration of the process that is now running
                 executingDuration = executingProcess.getDuration();
                 running = true;
 
             }
-
-
-                // increment currTime in each iteration
+            // increment currTime in each iteration
             currTime++;
             // if there is a process running, decrement the executingDuration
             // when this variable is 0, no process is running or one process just finished running
@@ -199,13 +219,14 @@ class ProcessScheduling {
         //if there is a process running, add the remaining time of execution to currTime and complete the last process.
         if(executingDuration!= 0) {
             currTime += executingDuration;
-            completeExecution(currTime, maxWait, executingProcess, Q);
+            completeExecution(currTime, maxWait, executingProcess, Q, myWriter);
         }
         double averageWaitTime = totalWaitTime/numProcesses;
-        System.out.println("\nTotal wait time = " + totalWaitTime);
-        System.out.println("Average wait time = " + averageWaitTime);
+        myWriter.write("\nTotal wait time = " + totalWaitTime);
+        myWriter.write("Average wait time = " + averageWaitTime);
 
-
+        //close output file
+        myWriter.close();
 
     }
 }
